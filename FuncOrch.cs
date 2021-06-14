a8f0766642ab6a7ccdd0f7d6711a1d080c981a53
Bart Czernicki <bartczernicki@outlook.com>
2021-06-14 14:57:25 -0400

    Removed empty code

--- FuncOrch.cs
+++ FuncOrch.cs
@@ -40,112 +40,6 @@ namespace MLNetTrainingDurableFunctions
             /*, "MVPs", "TripleCrowns", "GoldGloves", "MajorLeaguePlayerOfTheYearAwards"*/
         };
 
-        //[FunctionName("FuncOrch")]
-        //public static async Task<List<string>> RunOrchestrator(
-        //    [OrchestrationTrigger] IDurableOrchestrationContext context)
-        //{
-        //    var outputs = new List<string>();
-
-        //    // Replace "hello" with the name of your Durable Activity Function.
-        //    outputs.Add(await context.CallActivityAsync<string>("FuncOrch_Hello", "Tokyo"));
-        //    outputs.Add(await context.CallActivityAsync<string>("FuncOrch_Hello", "Seattle"));
-        //    outputs.Add(await context.CallActivityAsync<string>("FuncOrch_Hello", "London"));
-
-        //    // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
-        //    return outputs;
-        //}
-
-        //[FunctionName("FuncOrch_Hello")]
-        //public static string SayHello([ActivityTrigger] string name, ILogger log)
-        //{
-        //    log.LogInformation($"Saying hello to {name}.");
-        //    return $"Hello {name}!";
-        //}
-
-        //[FunctionName("FuncOrch_HttpStart")]
-        //public static async Task<HttpResponseMessage> HttpStart(
-        //    [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestMessage req,
-        //    [DurableClient] IDurableOrchestrationClient starter,
-        //    ILogger log)
-        //{
-        //    // Function input comes from the request content.
-        //    string instanceId = await starter.StartNewAsync("E2_BackupSiteContent", null);
-
-        //    log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
-
-        //    return starter.CreateCheckStatusResponse(req, instanceId);
-        //}
-
-        //[FunctionName("E2_BackupSiteContent")]
-        //public static async Task<long> Run([OrchestrationTrigger] IDurableOrchestrationContext backupContext)
-        //{
-        //    string rootDirectory = backupContext.GetInput<string>()?.Trim();
-        //    if (string.IsNullOrEmpty(rootDirectory))
-        //    {
-        //        rootDirectory = Directory.GetParent(typeof(FuncOrch).Assembly.Location).FullName;
-        //    }
-
-        //    string[] files = await backupContext.CallActivityAsync<string[]>(
-        //        "E2_GetFileList",
-        //        rootDirectory);
-
-        //    if (memoryCacheTest.GetCount() == 0)
-        //    {
-        //        CacheItemPolicy policy = new CacheItemPolicy();
-        //        memoryCacheTest.Set("files", value: files, policy);
-        //    }
-
-        //    var tasks = new Task<long>[files.Length];
-        //    for (int i = 0; i < files.Length; i++)
-        //    {
-        //        tasks[i] = backupContext.CallActivityAsync<long>(
-        //            "E2_CopyFileToBlob",
-        //            files[i]);
-        //    }
-
-        //    await Task.WhenAll(tasks);
-
-        //    long totalBytes = tasks.Sum(t => t.Result);
-        //    return totalBytes;
-        //}
-
-        //[FunctionName("E2_GetFileList")]
-        //public static string[] GetFileList([ActivityTrigger] string rootDirectory, ILogger log)
-        //{
-        //    log.LogInformation($"Searching for files under '{rootDirectory}'...");
-        //    string[] files = Directory.GetFiles(rootDirectory, "*", SearchOption.AllDirectories);
-        //    log.LogInformation($"Found {files.Length} file(s) under {rootDirectory}.");
-
-        //    return files;
-        //}
-
-        //[FunctionName("E2_CopyFileToBlob")]
-        //public static async Task<long> CopyFileToBlob([ActivityTrigger] string filePath, Microsoft.Azure.WebJobs.Binder binder, ILogger log)
-        //{
-        //    string[] fileNames = memoryCacheTest["files"] as string[];
-        //    log.LogInformation($"file names:'{fileNames.Length}'");
-
-        //    long byteCount = new FileInfo(filePath).Length;
-
-        //    // strip the drive letter prefix and convert to forward slashes
-        //    string blobPath = filePath
-        //        .Substring(Path.GetPathRoot(filePath).Length)
-        //        .Replace('\\', '/');
-        //    string outputLocation = $"backups/{blobPath}";
-
-        //    //log.LogInformation($"Copying '{filePath}' to '{outputLocation}'. Total bytes = {byteCount}.");
-
-        //    // copy the file contents into a blob
-        //    using (Stream source = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.Read))
-        //    using (Stream destination = await binder.BindAsync<CloudBlobStream>(
-        //        new BlobAttribute(outputLocation, FileAccess.Write)))
-        //    {
-        //        await source.CopyToAsync(destination);
-        //    }
-
-        //    return byteCount;
-        //}
-
         [FunctionName("BaseballFunc_HttpStart")]
         public static async Task<HttpResponseMessage> HttpStart(
         [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestMessage req,
@@ -279,8 +173,6 @@ namespace MLNetTrainingDurableFunctions
 
                 return string.Empty;
             }
-
-
         }
     }
 }
